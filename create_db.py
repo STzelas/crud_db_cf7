59a01cfe8c27566c7ca00e6c9d75504e62cd8eb8
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-15 18:07:57 +0300

    Initial commit

---
+++ create_db.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password):
+  connection = None
+
+  try:
+    connection = mysql.connector.connect(
+      host = host_name,
+      user = user_name,
+      password = user_password
+    )
+
+    print("Connection to MySQL DB successful.")
+  except Error as e:
+    print(f"Error: {e} occurred")
+    
+  return connection
